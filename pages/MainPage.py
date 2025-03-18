43ec42138e8dd45d0f83b6a9c8078767c44c6176
Kliuev Dmitrii <dimik1986@gmail.com>
2025-03-18 16:12:21 +0400

    add_delete_board

--- pages/MainPage.py
+++ pages/MainPage.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 from configuration.ConfigProvider import ConfigProvider
 from testdata.DataProvider import DataProvider
@@ -21,7 +22,8 @@ class MainPage:
     @allure.step("Открыть боковое меню")
     def open_menu(self):
         WebDriverWait(self.__driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "button[data-testid='header-member-menu-button']"))
+            EC.element_to_be_clickable((By.CSS_SELECTOR,
+                "button[data-testid='header-member-menu-button']"))
             ).click()
 
     @allure.step("Прочитать информацию о пользователе")    
@@ -44,31 +46,68 @@ class MainPage:
     @allure.step("Добавить заголовок доски")
     def add_name_board(self, name_board):
         title_input = WebDriverWait(self.__driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "input[data-testid='create-board-title-input']"))
+            EC.presence_of_element_located((By.CSS_SELECTOR, 
+                "input[data-testid='create-board-title-input']"))
             )
         title_input.send_keys(name_board)
 
     @allure.step("Нажать кнопку 'Создать'")
     def click_button_create(self):
         create_button = WebDriverWait(self.__driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "button[data-testid='create-board-submit-button']"))
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 
+                "button[data-testid='create-board-submit-button']"))
             )
         create_button.click()
 
     @allure.step("Нажать кнопку 'Доски' в меню")
     def click_button_boards(self):
         button_boards = WebDriverWait(self.__driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//p[@class='nNvJhnERHVQb9o' and text()='Доски']"))
+            EC.element_to_be_clickable((By.XPATH, 
+                "//p[@class='nNvJhnERHVQb9o' and text()='Доски']"))
             )
         button_boards.click()
 
     @allure.step("Получить список досок")
     def get_boards_info(self) -> list[str]:
-        container = WebDriverWait(self.__driver, 10).until(
+        try:
+            container = WebDriverWait(self.__driver, 5).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "ul.L0brLYR3ZZ6wm4"))
+        )
+            boards = container.find_elements(By.CSS_SELECTOR, "div.LinesEllipsis")
+            return [board.text for board in boards]
+        except TimeoutException:
+            return []
+    
+    @allure.step("Открыть меню созданной доски")
+    def click_menu_board(self):
+        button_menu_board = WebDriverWait(self.__driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, 
+                "//button[@aria-label='Меню']"))
             )
-        boards = container.find_elements(By.CSS_SELECTOR, "div.LinesEllipsis")
-        boards_names = [board.text for board in boards]
-        return boards_names
+        button_menu_board.click()
 
+    @allure.step("Выбрать 'Закрыть доску'")
+    def click_closed_board(self):
+        closed_board = WebDriverWait(self.__driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, 
+                "//div[contains(@class, 'S1YMKJFPn9WNGk') and text()='Закрыть доску']"))
+            )
+        closed_board.click()
+        button_closed_board = WebDriverWait(self.__driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, 
+                "//button[@data-testid='popover-close-board-confirm']"))
+            )
+        button_closed_board.click()
 
+    @allure.step("Выбрать 'Удалить доску навсегда'")
+    def click_delete_board(self):
+        delete_board = WebDriverWait(self.__driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, 
+                "//button[@data-testid='close-board-delete-board-button']"))
+            )
+        delete_board.click()
+        button_delete_board = WebDriverWait(self.__driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, 
+                "//button[@data-testid='close-board-delete-board-confirm-button']"))
+            )
+        button_delete_board.click()
--- test/ui/boards_test.py
+++ test/ui/boards_test.py
@@ -30,3 +30,26 @@ def add_new_board_test(browser, test_data: dict):
 
     with allure.step("Проверить, что создана доска с названием "+ name_board):
         assert info[0] == name_board
+
+def delete_new_board_test(browser, test_data: dict):
+    username = test_data.get("username")
+    email = test_data.get("email")
+    password = test_data.get("pass")
+    name_board = fake.word()
+
+    auth_page = AuthPage(browser)
+    auth_page.go()
+    auth_page.login_as(email, password)
+    WebDriverWait(browser, 20).until(EC.url_contains("kliuev_dmitrii/boards"))
+
+    main_page = MainPage(browser)
+    main_page.click_button_create_board()
+    main_page.add_name_board(name_board)
+    main_page.click_button_create()
+    main_page.click_menu_board()
+    main_page.click_closed_board()
+    main_page.click_delete_board()
+    info = main_page.get_boards_info()
+    
+    with allure.step(f"Проверка, что доска '{name_board}' удалена"):
+        assert name_board not in info, f"Ошибка: Доска '{name_board}' не была удалена!"
