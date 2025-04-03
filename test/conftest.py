9b76b20f510e4074f5cbc18670f746ecd4fce2b5
Kliuev Dmitrii <dimik1986@gmail.com>
2025-04-03 22:46:32 +0400

    ref

--- test/conftest.py
+++ test/conftest.py
@@ -4,6 +4,8 @@ import allure
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
